023cb2e703cbd0ed1bb2a32593f947f90b536629
zsyie <48537915+zsyie@users.noreply.github.com>
2019-03-19 12:39:00 +0800

    Update Client.c

--- Client.c
+++ Client.c
@@ -38,7 +38,7 @@ int main(int argc, char const *argv[])
         return -1; 
     } 
     send(sock , hello , strlen(hello) , 0 ); 
-    printf("Ohaiyo gozaimasu"); 
+    printf("Annyonghaseyo"); 
     valread = read( sock , buffer, 1024); 
     printf("%s\n",buffer ); 
     return 0; 
